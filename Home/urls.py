79ea13666fa9f7b6b6d676b1cf05fbe6f5182ec9
Mahesh Das <Mahesh.das2205@gmail.com>
2025-07-18 01:49:41 +0530

    Add welcome view and template, update home URL routing

--- Home/urls.py
+++ Home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.welcome, name="welcome"),
+    path('home/', views.home, name="home"),
 ]
