d3be2894498963fd65cbba03907c93c729ea63d8
Mahesh Das <Mahesh.das2205@gmail.com>
2025-07-19 03:57:54 +0530

    initialized base.html with ui changes

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def signup(request):
     if request.method=="POST":
-        username=request.POST.get
+        firstname=request.POST.get("firstname")
+        lastname=request.POST.get("lastname")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        cfpassword=request.POST.get("confirmpassword")
+
+        if password != cfpassword:
+            return render(request,"accounts/signup.html",{"error":"Passwords do not Match enter password again"})
     return render(request,"accounts/signup.html")
 def login(request):
     if request.method=="POST":
