5b67a44b70fadbfd326e8041b58c7ba60bd5c358
Mahesh Das <Mahesh.das2205@gmail.com>
2025-07-21 12:32:49 +0530

    changes in UI design removed welcome page now left with only home page

--- Home/urls.py
+++ Home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name="welcome"),
-    path('home/', views.home, name="home"),
+    # path('', views.welcome, name="welcome"),
+    path('', views.home, name="home"),
 ]
