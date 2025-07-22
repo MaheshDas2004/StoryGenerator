d85e3f9c7f8f75024baf611ba2a2e4ffccd3a7d2
Mahesh Das <Mahesh.das2205@gmail.com>
2025-07-22 18:46:52 +0530

    implemented demo create story app

--- stories/views.py
+++ stories/views.py
@@ -5,11 +5,23 @@ from google import genai
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 def createstory(request):
-    client = genai.Client()
+    if request.method=="POST":
+        mc=request.POST.get("characterName")
+        age=request.POST.get("age")
+        genre=request.POST.getlist("genre")
+        story_set=request.POST.get("setting")
+        length=request.POST.get("length")
+        sp_ele=request.POST.getlist("elements")
 
-    response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="give a story on king and queens",)
-
-    print(response.text)
+        prompt=f"""
+                Generate a short children’s story featuring a main character named {mc}, who is {age} years old.
+                The story should fall under the following genres: {', '.join(genre)}.
+                Set the story in a {story_set} environment, and keep the story approximately {length} long.
+                Incorporate the following special elements: {', '.join(sp_ele)}.
+                Make it age-appropriate and engaging.
+                """
+        client = genai.Client()
+        response = client.models.generate_content(model="gemini-2.5-flash",contents=prompt,)
+  
+        return render(request,"stories/createstory.html",{"story":response.text})
     return render(request,"stories/createstory.html")
