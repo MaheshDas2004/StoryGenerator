c4fb83ca55d71c11874e158b20fef0338b1d3704
Mahesh Das <Mahesh.das2205@gmail.com>
2025-07-25 18:07:53 +0530

    added logic for audio generation

--- stories/views.py
+++ stories/views.py
@@ -3,9 +3,21 @@ from django.contrib.auth.decorators import login_required
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import wave
+
+def wave_file(filename, pcm, channels=1, rate=24000, sample_width=2):
+   with wave.open(filename, "wb") as wf:
+      wf.setnchannels(channels)
+      wf.setsampwidth(sample_width)
+      wf.setframerate(rate)
+      wf.writeframes(pcm)
+
+
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 @login_required
+
 def createstory(request):
     if request.method=="POST":
         mc=request.POST.get("characterName")
@@ -54,7 +66,27 @@ def createstory(request):
         title = client.models.generate_content(model="gemini-2.5-flash", contents=prompt_title)
         story = client.models.generate_content(model="gemini-2.5-flash", contents=prompt_story)
         moral = client.models.generate_content(model="gemini-2.5-flash", contents=prompt_moral)
-        elements={}
-    
+
         return render(request,"stories/createstory.html",{"story":story.text,"title":title.text,"moral":moral.text})
     return render(request,"stories/createstory.html")
+
+def generateAudio(request):
+    client = genai.Client()
+    response = client.models.generate_content(
+    model="gemini-2.5-flash-preview-tts",
+    contents=f"say as if you are narrating story to children:\n\n{xyz}",
+    config=types.GenerateContentConfig(
+        response_modalities=["AUDIO"],
+        speech_config=types.SpeechConfig(
+            voice_config=types.VoiceConfig(
+                prebuilt_voice_config=types.PrebuiltVoiceConfig(
+                voice_name='Sulafat',
+                )
+            )
+        ),
+    )
+    )
+    data = response.candidates[0].content.parts[0].inline_data.data
+
+    file_name='out.wav'
+    wave_file(file_name, data) # Saves the file to current directory
